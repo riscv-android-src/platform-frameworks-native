3bccb0ac61d5ffc54f78be7fb26fde4d666f9a55
haocheng.zy <haocheng.zy@linux.alibaba.com>
2022-09-13 15:55:47 +0800

    FIXME: Remove dex2oat32 test case for riscv 64bit only
    Change-Id: Ib5b0f1d3b8f7983d20aeb1235d64a590028d5135

--- cmds/installd/tests/installd_dexopt_test.cpp
+++ cmds/installd/tests/installd_dexopt_test.cpp
@@ -818,6 +818,9 @@ TEST_F(DexoptTest, DexoptDex2oat64Enabled) {
         android::base::SetProperty(property, previous_value);
     });
     std::string odex = GetPrimaryDexArtifact(app_oat_dir_.c_str(), apk_path_, "odex");
+#if (defined(__riscv) && (__riscv_xlen == 64))
+    // 64bit only has no dex2oat32
+#else
     // Disable the property and use dex2oat32.
     ASSERT_TRUE(android::base::SetProperty(property, "false")) << property;
     CompilePrimaryDexOk("speed-profile",
@@ -828,6 +831,7 @@ TEST_F(DexoptTest, DexoptDex2oat64Enabled) {
                         DEX2OAT_FROM_SCRATCH,
                         /*binder_result=*/nullptr,
                         empty_dm_file_.c_str());
+#endif
     // Enable the property and use dex2oat64.
     ASSERT_TRUE(android::base::SetProperty(property, "true")) << property;
     CompilePrimaryDexOk("speed-profile",
